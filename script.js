bcd4768c020a00643512015a8d2afce6a947eb52
hshahid <hjshahid80@gmail.com>
2020-05-22 14:41:35 -0500

    Updated Dom Lab

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
-let button = document.createElement('button');
-let buttonText = document.createTextNode('Add Square');
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.createElement("button");
+  let buttonText = document.createTextNode("Add Square");
+  let colorArray = ["red", "blue", "green", "purple","grey","pink"];
 
-button.appendChild(buttonText);
-document.body.appendChild(button);
-button.addEventListener('click', addDiv);
-let idNum = 1
+  button.appendChild(buttonText);
+  document.body.appendChild(button);
+  button.addEventListener("click", addDiv);
+  let idNum = 1;
 
-function addDiv() {
-    let div = document.createElement('div');
-    div.classList = ('squares');
+  function addDiv() {
+    let div = document.createElement("div");
+    div.classList = "squares";
     document.body.appendChild(div);
-div.setAttribute('id', idNum); 
-idNum++
-div.addEventListener('mouseenter', function() {
-    div.textContent = div.id;
-})
- div.addEventListener('mouseleave', function() {
-     div.textContent = ' ';
-})
+    div.setAttribute("id", idNum);
+    idNum++;
+    div.addEventListener("mouseenter", function () {
+      div.textContent = div.id;
+    });
+    div.addEventListener("mouseleave", function () {
+      div.textContent = " ";
+    });
+    div.addEventListener('click', function() { 
+    div.style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+    } )
+  div.addEventListener('dblclick', function() {
+      if(div.id % 2 === 0) {
+          if(div.nextElementSibling){
+              div.nextElementSibling.remove();
+          } else{
+              alert('Nothing after this.');
+          }
+      }
+      if(div.id & 2 == 1) { 
+        if(div.previousElementSibling) 
+        div.previousElementSibling.remove();
+    } else {
+        alert('Nothing before this.');
+    }
+  } )
+      
+  
 }
-}
+});
+
+
